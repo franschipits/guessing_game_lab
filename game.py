3809f3a31f3624e4623f7e7bfed6dda308066039
Franciane Schipits <franmifreitas@gmail.com>
2023-08-28 18:48:19 -0500

    Choose random number

--- game.py
+++ game.py
@@ -5,4 +5,25 @@ print("hi")
 print("Greetings player")
 name = input("please enter your name:")
 print(name)
+print("Choose random number between 1 and 100")
 
+# while True: 
+#     number = input("number: ")
+#     num = random.randint(1, 101)
+#     if number != num:
+#         print(num)
+#     if number == num:
+#         break
+
+while True: 
+    guesses = 0
+    number = input("number: ")
+    num = random.randint(1, 101)
+    if number != num:
+        if number > num:
+            print("Lower")
+        if number < num:
+            print("Higher")
+        guesses + 1
+    if number == num:
+        print(f"Congratulations! You got it in {guesses}")
