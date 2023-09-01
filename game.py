db9296a17e107c8336f41be07d1b4a414188aa0c
Franciane Schipits <franmifreitas@gmail.com>
2023-08-31 20:05:52 -0500

    end

--- game.py
+++ game.py
@@ -1,30 +1,34 @@
 """A number-guessing game."""
 import random
 # Put your code here
-print("hi")
-print("Greetings player")
-name = input("please enter your name:")
-print(name)
+print("Hi!")
+print("Greetings player!")
+name = input("Please enter your name: ")
+print(f"Hi! {name}")
 print("Choose random number between 1 and 100")
 
-# while True: 
-#     number = input("number: ")
-#     num = random.randint(1, 101)
-#     if number != num:
-#         print(num)
-#     if number == num:
-#         break
-
-while True: 
+#num = random.randint(1, 101)
+#guesses = 0
+def guessing_game():
     guesses = 0
-    number = int(input("number: "))
     num = random.randint(1, 101)
-    if number != num:
-        if number > num:
-            print("Lower")
-        if number < num:
-            print("Higher")
-        guesses + 1
-    if number == num:
-        print(f"Congratulations! You got it in {guesses}")
-        break
+    while True: 
+        #where user inputs 
+        #check if input is just numbers
+        #if contains letters : input wrong guess again 
+        #else convert to int
+        number = input("number: ")
+        if number.isalpha():
+            print("Type a number...")
+        else:
+            number = int(number)
+            if number != num:
+                if number > num:
+                    print("Lower!")
+                if number < num:
+                    print("Higher!")
+            guesses = guesses + 1
+            if number == num:
+                print(f"Congratulations! You got it in {guesses} guesses")
+                break
+guessing_game()
